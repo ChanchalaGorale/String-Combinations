052c5d9d3fc950bc9879738673bebdbb69bc0eb5
Chanchala G <cgorale111@gmail.com>
2021-01-27 01:25:06 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-function combine(str){
-    const result = [];
-    for(let i = 1; i < Math.pow(2, str.length) - 1; i++)
-       result.push([...str].filter((_, pos) => (i >> pos) & 1).join(""));
-   
-   return result;
- }
- 
- console.log(combine("abcd"));
+function combine(str) {
+  const result = [];
+  let unique = [];
+  let sortedresult = [];
+
+  for (let i = 1; i < Math.pow(3, str.length) - 1; i++) {
+    result.push([...str].filter((_, pos) => (i >> pos) & 1).join(""));
+
+    unique = [...new Set(result)];
+
+    sortedresult = unique.sort();
+  }
+
+  return sortedresult;
+}
+
+function generator(str) {
+  let newarray = combine(str);
+
+  const final = [];
+
+  for (let i = 0; i < newarray.length; i++) {
+    if (newarray[i] !== "") {
+      final.push(newarray[i]);
+    }
+  }
+
+  return final;
+}
+
+function valueprovider(str) {
+  let myarray = generator(str);
+
+  for (let i = 0; i < myarray.length; i++) {
+    console.log(myarray[i]);
+  }
+}
+
+valueprovider("abcd");
